52e46c944bc578417262facd10cd84c2003393ab
susamgunes <gunessusam01@gmail.com>
2023-12-10 22:57:54 +0300

    migrate okey

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class GeneralSettings(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True, 
+    )
+    
+    description = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+    )
+    parameter = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+    )
+    updated_date=models.DateTimeField(
+        blank=True,
+        auto_now=True,
+    )
+    creted_date=models.DateField(
+        blank=True,
+        auto_now_add=True,
+    )
+    
+
