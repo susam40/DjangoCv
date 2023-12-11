8a4cc328a88daee62a6feec68435ad4664805c1e
susamgunes <gunessusam01@gmail.com>
2023-12-11 23:32:28 +0300

    image file okey

--- core/admin.py
+++ core/admin.py
@@ -9,4 +9,12 @@ class GeneralSettingAdmin(admin.ModelAdmin):
     list_editable=['description','parameter']
     class Meta:
         model = GeneralSetting
+        
+@admin.register(ImageSetting) 
+class ImageSettingAdmin(admin.ModelAdmin):
+    list_display=['id','name','description','file','updated_date','created_date']
+    search_fields=['name','description','file']
+    list_editable=['description','file']
+    class Meta:
+        model = ImageSetting
 
--- core/models.py
+++ core/models.py
@@ -34,5 +34,43 @@ class GeneralSetting(models.Model):
         verbose_name = 'General Setting'
         verbose_name_plural = 'General Settings'
         ordering = ('name', )
+        
+class ImageSetting(models.Model):
+        name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True, 
+        verbose_name='Name Test',
+        help_text='Ayar İsmi'
+    )
+        description = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+    )
+        file = models.ImageField(
+            default='',
+            verbose_name='Image',
+            help_text='',
+            blank=True,
+            upload_to='images/',
+        )
+        
+        updated_date=models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        )
+        
+        created_date=models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        
+        )
+        
+        def __str__(self):
+            return 'Image Setting:'+self.name
     
-
+        class Meta:
+            verbose_name = 'Image Setting'
+            verbose_name_plural = 'Image Setting'
+            ordering = ('name', )
